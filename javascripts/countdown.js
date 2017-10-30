a8c9eae6fb4ad29348344377b7335879e37bf17d
pupreti <latitudemt8848@gmail.com>
2017-10-30 01:46:50 -0400

    base url change

--- javascripts/countdown.js
+++ javascripts/countdown.js
@@ -1,3 +1,3 @@
-$.get( "https://explorer.vertcoin.org/api/getblockcount", function( data ) {
+$.get("https://explorer.vertcoin.org/api/getblockcount", function( data ) {
     alert( "Data Loaded: " + data );
 });
